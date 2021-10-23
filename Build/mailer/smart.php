ba497b6d43efd1ce3a093a9f38be3e3cee547c48
Roman Popil <romapopil5278@gmail.com>
2021-10-23 09:43:20 +0300

    Small cosmetic changes

--- Build/mailer/smart.php
+++ Build/mailer/smart.php
@@ -16,15 +16,15 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'avisohelabot@gmail.com';                 // Наш логин
-$mail->Password = 'avisohelabutbot2021';                              // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                              // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
 
 $email->From = $email;
 $email->FromName = $name;
 $mail->setFrom($email, $name);   // От кого письмо
-$mail->addAddress('avisohelabud@gmail.com');     // Add a recipient
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
